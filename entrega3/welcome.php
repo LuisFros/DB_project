8fec69f2e736d5ec1b51a1d7c9cef5f09394b6b7
malopez16 <malopez16@uc.cl>
2018-05-15 16:06:58 -0300

    everything in order

--- entrega3/welcome.php
+++ entrega3/welcome.php
@@ -1,19 +1,11 @@
-<!DOCTYPE html>
 <html>
-<head>
+<body>
 
-// LOGIN
+$uid = $_POST["UserID"];
+$email = $_POST["email"];
 
-<form action="login.php" method="post">
-UserID: <input type="text" name="uid"><br>
-Password: <input type="text" name="password"><br>
-<input type="submit">
-</form>
+Welcome <?php echo $uid; ?><br>
+Your email address is: <?php echo $email; ?>
 
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////
- 
-
-    </div>
-  </body>
+</body>
 </html>

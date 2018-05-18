fbd55dd2f0ca0eed972b232e1bceaed84a4415ff
Mauro <malopez16@uc.cl>
2018-05-17 21:11:25 -0400

    added registering form

--- Sites/Entrega3/index.php
+++ Sites/Entrega3/index.php
@@ -1,11 +1,28 @@
-<html>
+<?php
+// Initialize the session
+session_start();
+
+// If session variable is not set it will redirect to login page
+if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
+  header("location: login.php");
+  exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
 <body>
-
-<form action="login.php" method="post">
-Usuario: <input type="text" name="name"><br>
-Password: <input type="password" name="password"><br>
-<input type="submit">
-</form>
-
+    <div class="page-header">
+        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION['username']); ?></b>. Welcome to our site.</h1>
+    </div>
+    <p><a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a></p>
 </body>
 </html>
